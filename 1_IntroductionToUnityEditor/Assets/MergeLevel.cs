cadd54a7bfeba1d72d69f2bc89e46da5d6ed722c
Rob <robertlockyer@gmail.com>
2018-10-14 18:16:29 -0230

    fixed the coordinate system of the modular assets so they have the correct orientation in the asset browser, set the scene up to use baked lighting. This is something I'll probably break out into a seperate project in order to keep the first lesson simple

---
+++ 1_IntroductionToUnityEditor/Assets/MergeLevel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MergeLevel : MonoBehaviour {
+
+    void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Start");
+    }
+
+    [SerializeField]
+    bool once = true;
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if (once)
+        //{
+        //    CombineInstance[] combine = new CombineInstance[transform.childCount];
+        //    for (int i = 1; i < transform.childCount; i++)
+        //    {
+        //        Transform childTransform = transform.GetChild(i);
+        //        combine[i].mesh = childTransform.gameObject.GetComponent<MeshFilter>().sharedMesh;
+        //        combine[i].transform = childTransform.localToWorldMatrix;
+        //        childTransform.gameObject.SetActive(false);
+        //    }
+
+        //    MeshFilter meshFilter = transform.GetChild(0).GetComponent<MeshFilter>();
+        //    meshFilter.mesh.CombineMeshes(combine);
+        //    transform.gameObject.SetActive(true);
+        //}
+        //once = false;
+    }
+}
