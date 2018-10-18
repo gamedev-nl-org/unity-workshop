4f0d8e2efce703404d5b696651aacd0fa554b8dd
Rob <robertlockyer@gmail.com>
2018-10-18 20:59:33 -0230

    added the simplest player controller I could think of so that I can demonstrate running the editor and editing properties in realtime. Doing this required rebuilding the level with colliders as well.

---
+++ Workshop_1/Assets/Player/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    float movementSpeed = 200.0f;
+
+    [SerializeField]
+    float rotateSpeed = 4.0f;
+	
+	void Update () {
+        Vector3 movement = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movement -= transform.forward;
+        }
+
+        GetComponent<CharacterController>().SimpleMove(movement * Time.deltaTime * movementSpeed);
+
+        Vector3 orientation = Vector3.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            orientation -= transform.right;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            orientation += transform.right;
+        }
+
+        transform.forward = Vector3.Lerp(transform.forward, orientation.normalized, rotateSpeed * Time.deltaTime);
+    }
+}
