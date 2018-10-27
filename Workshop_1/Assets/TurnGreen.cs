530287cebf567d75b7e704ab93eb9c4142f7b4a8
Rob <robertlockyer@gmail.com>
2018-10-26 21:59:48 -0230

    added a modified version of the script from the tutorial for changing the cube and wall colors.

---
+++ Workshop_1/Assets/TurnGreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnGreen : MonoBehaviour {
+
+    [SerializeField]
+    Color enterColor = Color.green;
+
+    [SerializeField]
+    Color exitColor = Color.red;
+
+    [SerializeField]
+    GameObject coloredWalls;
+
+    Color oldWallColor;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<Renderer>().material.color = enterColor;
+        oldWallColor = coloredWalls.GetComponent<Renderer>().material.color;
+        coloredWalls.GetComponent<Renderer>().material.color = enterColor;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.GetComponent<Renderer>().material.color = exitColor;
+        coloredWalls.GetComponent<Renderer>().material.color = oldWallColor;
+    }
+}
