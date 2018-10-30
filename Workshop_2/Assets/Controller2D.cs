49a809fb2aaf01fb5997dd67dfe94a2045727752
Michael G. Burton <michaelgburton@hotmail.com>
2018-10-30 07:29:23 -0230

    Adding second workshop "so far" Rebinding prefabs to new copies to allow illustration of original/new behaviours Added 2D colliders to most objects Added new prefab WideDoor_Final to allow some interactive box collider changes Added basic 2D controller Modified lighting setup Rotated level into x-y plane

---
+++ Workshop_2/Assets/Controller2D.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller2D : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var horizontal = Input.GetAxis("Horizontal") * transform.right;
+        var vertical = Input.GetAxis("Vertical") * transform.up;
+		GetComponent<Rigidbody2D>().AddForce(horizontal + vertical);
+	}
+}
