ffa3c4aeaa915f7952cbe5b37abfed6cfa627fdc
Rob <robertlockyer@gmail.com>
2018-11-05 17:44:44 -0330

    setup doors

---
+++ Workshop_4/Assets/Level/Door/Door.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject door;
+
+    [SerializeField]
+    bool isDoorOpened = false;
+
+    bool enteredArea = false;
+
+    float currentRotation;
+
+    float startingRotation;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        enteredArea = true;
+    }
+
+    private void Start()
+    {
+        startingRotation = door.transform.eulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(enteredArea)
+        {
+            GetComponent<BoxCollider>().enabled = false;
+
+            if(currentRotation < 180)
+            {
+                currentRotation = startingRotation + 100.0 * Time.deltaTime;
+                if(currentRotation > 180)
+                {
+                    currentRotation = 180;
+                }
+                door.transform.eulerAngles = new Vector3(door.transform.eulerAngles.x, doorRotation, door.transform.eulerAngles.z);
+            }
+
+        }
+	}
+}
