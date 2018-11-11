e4e69e4b11d79ce1a382e0d1450e75c0f5403f11
Rob <robertlockyer@gmail.com>
2018-11-11 14:32:48 -0330

    set 2d physics gravity to 0, and started work on the player controls

--- Workshop_3/Assets/PlayerController.cs
+++ Workshop_3/Assets/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    Vector2 TargetVelocity = Vector3.zero;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,7 +13,13 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Input.GetAxis("Horizontal");
-        Input.GetAxis("Vertical");
-	}
+        TargetVelocity.x = Input.GetAxis("Horizontal");
+        TargetVelocity.y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody2D rigid = GetComponent<Rigidbody2D>();
+        rigid.velocity = TargetVelocity;
+    }
 }
