0b97b1f0fdc2b2c428d1667ff139cc90acb1cf23
Rob <robertlockyer@gmail.com>
2018-11-11 15:24:09 -0330

    added a camera controller

---
+++ Workshop_3/Assets/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField]
+    GameObject CameraTarget;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        Vector3 TargetPos = CameraTarget.transform.position;
+        TargetPos.z = transform.position.z;
+        transform.position = TargetPos;
+    }
+}
--- Workshop_3/Assets/PlayerController.cs
+++ Workshop_3/Assets/PlayerController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    Vector2 TargetVelocity = Vector3.zero;
+    Vector2 TargetVelocity = Vector2.zero;
+    Vector3 LookAtTarget = Vector3.zero;
+    RaycastHit2D[] results = new RaycastHit2D[16];
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -15,11 +17,25 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
         TargetVelocity.x = Input.GetAxis("Horizontal");
         TargetVelocity.y = Input.GetAxis("Vertical");
+
+        LookAtTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        LookAtTarget.z = 0;
+
+        //We might normally use the code below in a 3D game but...
+        //transform.LookAt(LookAtTarget); makes z face the target, therefore doesn't work in 2D
+        transform.right = LookAtTarget - transform.position;
     }
 
     private void FixedUpdate()
     {
         Rigidbody2D rigid = GetComponent<Rigidbody2D>();
-        rigid.velocity = TargetVelocity;
+        
+        rigid.velocity = TargetVelocity * 3.0f;
+        //rigid.Cast(rigid.velocity, results);
+        //if(results.Length > 0)
+        //{
+        //    rigid.velocity = Vector2.zero;
+        //}
+
     }
 }
